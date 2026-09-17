091e18bd8cf874f817cd42eb19b133d3e079d59c
wierzba-pl <trehubova.pl@gmail.com>
2026-09-17 08:41:09 +0200

    Update Meta Pixel ID

--- app/layout.jsx
+++ app/layout.jsx
@@ -33,7 +33,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1398978959079731');
+fbq('init', '1520144700138160');
 fbq('track', 'PageView');`
           }}
         />
@@ -42,7 +42,7 @@ fbq('track', 'PageView');`
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1398978959079731&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1520144700138160&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
